2a7d493c458af52c3ecc3da1413000d23a1332fb
tannerjaime <tannj583@gmail.com>
2016-03-17 11:44:15 -0400

    data structure example

---
+++ writing/sampleDataStructure.js
@@ -0,0 +1,23 @@
+{
+    "name": "Acacia abyssinicav",
+    "growthChar": {
+        "minTemperature": -14,
+        "minPrecipitation": 22,
+        "maxPrecipitation": 260,
+        "minPH": 4.8,
+        "maxPH": 7.2,
+        "toleranceFire": true,
+        "toleranceDrought": "Low",
+        "toleranceAnaerobic": "Medium"
+    },
+    "donorOrigin": {
+        "countryCode": "JPN",
+        "countryName": "Japan",
+        "continentName": "Asia",
+        "providerInstitute": "USA996",
+    },
+    "speciesOrigin": {
+        "countryOrigin": ["Madagascar", " Mascarene Islands"]
+        "climate": "arid",
+    }
+};
