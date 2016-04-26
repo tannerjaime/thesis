a5b57ed7e24423d7d2ffe7867df446607fe49497
tannerjaime <tannj583@gmail.com>
2016-04-25 21:03:46 -0400

    focus on one class

--- work/thesis.js
+++ work/thesis.js
@@ -1,5 +1,5 @@
 var request = require('request'); // npm install request
-var async = require('async'); // npm install async
+//var async = require('async'); // npm install async
 var fs = require('fs');
 
 var GBIFapiRequest = 'http://api.gbif.org/v1/species/search?q=Magnoliophyta&rank=SPECIES&limit=1000';
@@ -12,22 +12,22 @@ var nopes = [];
 var gbifDONE = false;
 
 //  Request all Cycadophyta species from the GBIF API 
-request(GBIFapiRequest, function(err, resp, body) {
-    if (err) {
-        throw err;
-    }
+fs.readFile('cycadopsida0.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    //obj = JSON.parse(data);
+
     for (var i = 0; i < 1000; i++) {
-        seedPlant.sciName = JSON.parse(body).results[i].species;
-        console.log(seedPlant.sciName);
-        if (seedPlant.sciName == "Juniperus saltuaria"){console.log("Check sub=" + checkSub.length + "array length=" + plants.length + "nopes length=" + nopes.length);}
+        seedPlant.sciName = JSON.parse(data).results[i].species;
+
         //if there is no species listed, skip
-        
         if (typeof seedPlant.sciName === 'undefined') {
-            console.log("not a species");
+            //console.log("not a species");
+            nopes.push(seedPlant.sciName);
         }
         else {
-            if (seedPlant.sciName.length < 5) {
-                console.log("too short");
+            if (seedPlant.sciName.length < 4) {
+                //console.log("too short");
+                nopes.push(seedPlant.sciName);
             }
             else {
                 //check against running list of species to see if it is a repeat
@@ -35,36 +35,161 @@ request(GBIFapiRequest, function(err, resp, body) {
                     //arr.indexOf('Jaime') 0 , if its not there, its -1
                     var space = seedPlant.sciName.indexOf(" ");
                     var species = seedPlant.sciName.substr(space + 1, 50);
-                    seedPlant.species = species;
-                    seedPlant.kingdom = JSON.parse(body).results[i].kingdom;
-                    seedPlant.phylum = JSON.parse(body).results[i].phylum;
-                    seedPlant.class = JSON.parse(body).results[i].class;
-                    seedPlant.order = JSON.parse(body).results[i].order;
-                    seedPlant.family = JSON.parse(body).results[i].family;
-                    seedPlant.genus = JSON.parse(body).results[i].genus;
-                    checkSub.push(seedPlant.sciName);
-                    plants.push({
-                        sciName: seedPlant.sciName,
-                        species: seedPlant.species,
-                        classification: {
-                            kingdom: seedPlant.kingdom,
-                            phylum: seedPlant.phylum,
-                            'class': seedPlant.class,
-                            order: seedPlant.order,
-                            family: seedPlant.family,
-                            genus: seedPlant.genus
-                        }
-                    });
+
+                    if (species == "spec." || species === "environmental sample" || typeof species === 'undefined') {
+                        nopes.push(seedPlant.sciName);
+                    }
+                    else {
+                        console.log(seedPlant.sciName);
+                        seedPlant.species = species;
+                        seedPlant.kingdom = JSON.parse(data).results[i].kingdom;
+                        seedPlant.phylum = JSON.parse(data).results[i].phylum;
+                        seedPlant.class = JSON.parse(data).results[i].class;
+                        seedPlant.order = JSON.parse(data).results[i].order;
+                        seedPlant.family = JSON.parse(data).results[i].family;
+                        seedPlant.genus = JSON.parse(data).results[i].genus;
+                        checkSub.push(seedPlant.sciName);
+                        plants.push({
+                            sciName: seedPlant.sciName,
+                            species: seedPlant.species,
+                            classification: {
+                                kingdom: seedPlant.kingdom,
+                                phylum: seedPlant.phylum,
+                                'class': seedPlant.class,
+                                order: seedPlant.order,
+                                family: seedPlant.family,
+                                genus: seedPlant.genus
+                            }
+                        });
+                    }
                 }
                 else {
                     nopes.push(seedPlant.sciName);
                 }
             }
         }
-
     }
-    console.log("Check sub=" + checkSub.length + "array length=" + plants.length + "nopes length=" + nopes.length);
-    gbifDONE = true;
+    console.log("Check sub=" + checkSub.length + "  array length=" + plants.length + "  nopes length=" + nopes.length);
+    fs.readFile('cycadopsida1000.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        //obj = JSON.parse(data);
+
+        for (var i = 0; i < 1000; i++) {
+            seedPlant.sciName = JSON.parse(data).results[i].species;
+
+            //if there is no species listed, skip
+            if (typeof seedPlant.sciName === 'undefined') {
+                //console.log("not a species");
+                nopes.push(seedPlant.sciName);
+            }
+            else {
+                if (seedPlant.sciName.length < 4) {
+                    //console.log("too short");
+                    nopes.push(seedPlant.sciName);
+                }
+                else {
+                    //check against running list of species to see if it is a repeat
+                    if (checkSub.indexOf(seedPlant.sciName) == -1) {
+                        //arr.indexOf('Jaime') 0 , if its not there, its -1
+                        var space = seedPlant.sciName.indexOf(" ");
+                        var species = seedPlant.sciName.substr(space + 1, 50);
+
+                        if (species == "spec." || species === "environmental sample" || typeof species === 'undefined') {
+                            nopes.push(seedPlant.sciName);
+                        }
+                        else {
+                            console.log(seedPlant.sciName);
+                            seedPlant.species = species;
+                            seedPlant.kingdom = JSON.parse(data).results[i].kingdom;
+                            seedPlant.phylum = JSON.parse(data).results[i].phylum;
+                            seedPlant.class = JSON.parse(data).results[i].class;
+                            seedPlant.order = JSON.parse(data).results[i].order;
+                            seedPlant.family = JSON.parse(data).results[i].family;
+                            seedPlant.genus = JSON.parse(data).results[i].genus;
+                            checkSub.push(seedPlant.sciName);
+                            plants.push({
+                                sciName: seedPlant.sciName,
+                                species: seedPlant.species,
+                                classification: {
+                                    kingdom: seedPlant.kingdom,
+                                    phylum: seedPlant.phylum,
+                                    'class': seedPlant.class,
+                                    order: seedPlant.order,
+                                    family: seedPlant.family,
+                                    genus: seedPlant.genus
+                                }
+                            });
+                        }
+                    }
+                    else {
+                        nopes.push(seedPlant.sciName);
+                    }
+                }
+            }
+        }
+        console.log("Check sub=" + checkSub.length + "  array length=" + plants.length + "  nopes length=" + nopes.length);
+        fs.readFile('cycadopsida2000.json', 'utf8', function(err, data) {
+            if (err) throw err;
+            //obj = JSON.parse(data);
+
+            for (var i = 0; i < 154; i++) {
+                seedPlant.sciName = JSON.parse(data).results[i].species;
+
+                //if there is no species listed, skip
+                if (typeof seedPlant.sciName === 'undefined') {
+                    //console.log("not a species");
+                    nopes.push(seedPlant.sciName);
+                }
+                else {
+                    if (seedPlant.sciName.length < 4) {
+                        //console.log("too short");
+                        nopes.push(seedPlant.sciName);
+                    }
+                    else {
+                        //check against running list of species to see if it is a repeat
+                        if (checkSub.indexOf(seedPlant.sciName) == -1) {
+                            //arr.indexOf('Jaime') 0 , if its not there, its -1
+                            var space = seedPlant.sciName.indexOf(" ");
+                            var species = seedPlant.sciName.substr(space + 1, 50);
+
+                            if (species == "spec." || species === "environmental sample" || typeof species === 'undefined') {
+                                nopes.push(seedPlant.sciName);
+                            }
+                            else {
+                                console.log(seedPlant.sciName);
+                                seedPlant.species = species;
+                                seedPlant.kingdom = JSON.parse(data).results[i].kingdom;
+                                seedPlant.phylum = JSON.parse(data).results[i].phylum;
+                                seedPlant.class = JSON.parse(data).results[i].class;
+                                seedPlant.order = JSON.parse(data).results[i].order;
+                                seedPlant.family = JSON.parse(data).results[i].family;
+                                seedPlant.genus = JSON.parse(data).results[i].genus;
+                                checkSub.push(seedPlant.sciName);
+                                plants.push({
+                                    sciName: seedPlant.sciName,
+                                    species: seedPlant.species,
+                                    classification: {
+                                        kingdom: seedPlant.kingdom,
+                                        phylum: seedPlant.phylum,
+                                        'class': seedPlant.class,
+                                        order: seedPlant.order,
+                                        family: seedPlant.family,
+                                        genus: seedPlant.genus
+                                    }
+                                });
+                            }
+                        }
+                        else {
+                            nopes.push(seedPlant.sciName);
+                        }
+                    }
+                }
+            }
+            console.log("Check sub=" + checkSub.length + "  array length=" + plants.length + "  nopes length=" + nopes.length);
+
+            fs.writeFileSync("./cycadopsida.json", JSON.stringify(plants));
+        });
+    });
 });
 
 
